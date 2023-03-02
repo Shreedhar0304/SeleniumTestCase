2d237a6dfed4284926d73ec866261efb691c302c
Thota Sridhar <v-tsridhar@microsoft.com>
2023-03-03 01:16:56 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTestCase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTestCase")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f2718e3-ef73-45be-a467-692389d2cd4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTestCase
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            try
+            {
+                int waitingTime = 3000;
+                By SigninButton = By.XPath("//a[@class='btn btn-primary']");
+                By AzureButton = By.XPath("//button[@class='btn btn-primary btn-line']");
+                By Signin = By.XPath("//div/small[contains(text(), 'microsoft.com')]");
+
+                IWebDriver webDriver = new EdgeDriver();
+
+                Thread.Sleep(waitingTime);
+
+                webDriver.Navigate().GoToUrl("https://ftqtysbdevfeedbackportal.powerappsportals.com/welcomefasttrack/");
+
+                Thread.Sleep(waitingTime);
+
+                webDriver.Manage().Window.Maximize();
+
+                Thread.Sleep(waitingTime);
+
+                webDriver.FindElement(SigninButton).Click();
+
+                Thread.Sleep(waitingTime);
+
+                webDriver.FindElement(AzureButton).Click();
+
+                Thread.Sleep(waitingTime);
+
+                webDriver.FindElement(Signin).Click();
+                Thread.Sleep(waitingTime);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
